93e8a1d5aabb4cb9f427130231d3571edcddac23
Lucas Souza da Fonte <lucasfonte@Lucass-MacBook-Pro.local>
2019-08-28 00:28:38 -0300

    module 03 up to date

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,80 @@
 import Appointment from '../models/Appointment';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import User from '../models/User';
+import File from '../models/File';
+import * as Yup from 'yup';
 
 class AppointmentController {
+    async index(req, res){
+        const appointments = await Appointment.findAll({
+            where: { user_id: req.userId, canceled_at: null},
+            order: ['date'],
+            attributes: ['id', 'date'],
+            include: [
+                {
+                    model: User,
+                    as:'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+
+                },
+            ],
+        });
+
+        return res.json(appointments);
+    }
     async store(req, res) {
-        return res.json();
+        const schema = Yup.object().shape({
+            provider_id: Yup.number().required(),
+            date: Yup.date().required(),
+        });
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const { provider_id, date } = req.body;
+        
+        const isProvider = await User.findOne({
+            where: { id: provider_id, provider: true },
+        });
+
+        if(!isProvider) {
+            return res.status(401).json({ error: 'Providers only' });
+        }
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if(isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Past dates are not allowed' });
+        }
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if(checkAvailability){
+            return res.status(400).json({ error: 'Appointment already taken' });
+        }
+
+
+        const appointment = await Appointment.create({
+            user_id: req.userId,
+            provider_id,
+            date,
+        });
+
+        return res.json(appointment);
     }
 }
 
