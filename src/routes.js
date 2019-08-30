d544145bf8c4c55f70c1170ccf41a0aa768d290e
Lucas Souza da Fonte <lucasfonte@Lucass-MacBook-Pro.local>
2019-08-30 11:31:13 -0300

    updated notifications and canceled

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 
 import  authMiddleware from './app/middlewares/auth';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 
 const routes = new Router();
@@ -25,8 +26,12 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
